a5b5aa53565761026d60975b9a0d1479ac5e1fae
Sam Morgan <s_morgan@me.com>
2013-07-08 13:26:08 +0100

    initial commit

---
+++ wp-content/themes/drewsymo-Foundation-2ef5908/single.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Single
+ *
+ * Loop container for single post content
+ *
+ * @package WordPress
+ * @subpackage Foundation, for WordPress
+ * @since Foundation, for WordPress 4.0
+ */
+
+get_header(); ?>
+
+    <!-- Main Content -->
+    <div class="large-9 columns large-centered" role="main">
+    
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'single' ); ?>
+			<?php endwhile; ?>
+			
+		<?php endif; ?>
+
+    </div>
+    <!-- End Main Content -->
+
+<?php get_footer(); ?>
