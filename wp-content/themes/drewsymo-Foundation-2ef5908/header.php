f6896a7d719c9ceda3f13eadab10b4561a4e168d
Sam Morgan <s_morgan@me.com>
2013-07-08 14:31:42 +0100

    Initial commit with SASS enabled

--- wp-content/themes/drewsymo-Foundation-2ef5908/header.php
+++ wp-content/themes/drewsymo-Foundation-2ef5908/header.php
@@ -19,6 +19,7 @@
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/devstyles.css" />
 
 <!-- Set the viewport width to device width for mobile -->
 <meta name="viewport" content="width=device-width" />
@@ -30,24 +31,15 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-	<nav class="top-bar">
-		<ul class="title-area">
-			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-		</ul>
-		<section class="top-bar-section">
-			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'foundation_page_menu', 'walker' => new foundation_navigation() ) ); ?>
-		</section>
-	</nav>
-
 	<?php $header =  get_header_textcolor();
 	if ( $header !== "blank" ) : ?>
 	<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
 		<div class="row">
 			<div class="large-4 hide-for-small columns wrapper" id="bannerwrap">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/wpbanner.png" />
 			</div>
 			<div class="large-4 small-12 columns" id="logowrap">
-				<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
+				<h2><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" /></a>
 			</div>
 			<div class="large-4 small-12 columns" id="navwrap">
 				<div class="row">
